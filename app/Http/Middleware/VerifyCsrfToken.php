9d49137e8588cc8f73ac6305f5bba4f4a013a992
Binaya <binayachaudari@live.com>
2018-07-15 20:07:29 +0545

    composer updated, written api for login and profile

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
+      //Self add to exculde csrf token on all the forms in api request
+      'api/*',
         //
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,45 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/login','apiControllers\apiLoginController@showLoginForm')->middleware('guest');
+    Route::POST('/login','apiControllers\apiLoginController@login')->middleware('guest');
+
+    Route::get('/register','Auth\RegisterController@showRegistrationForm');
+    Route::POST('/register','apiControllers\apiRegisterController@register');
+
+    Route::get('/{username}','apiControllers\apiProfileController@showProfile');
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+    //
+    // Route::get;
+    // Route::POST;
+});
